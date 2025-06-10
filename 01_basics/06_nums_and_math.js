8e599e42465f865e6df977b89e8ff5ec278e0ffe
mayankshrivastava147 <92111620+mayankshrivastava147@users.noreply.github.com>
2025-06-10 08:51:14 +0000

    numbers

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,31 @@
+// const balance = new Number(100)
+// const score = 400
+// console.log(balance)
+// console.log(score)
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+
+// let otherNumber = 2931.8966
+
+// console.log(otherNumber.toPrecision(3))
+
+// const hundred = 1000000000
+
+// console.log(hundred.toLocaleString('en-IN'))
+
+
+//*****************************MATHS**********************************/
+
+// console.log(Math)
+
+// console.log(Math.abs(4));
+// console.log(Math.ceil(4.3))
+// console.log(Math.floor(4.9))
+
+// console.log(Math.random())
+
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max-min + 1)) + min )
