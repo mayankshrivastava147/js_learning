5926c210e02acac137563d6efcb2dd20007b446e
mayankshrivastava147 <92111620+mayankshrivastava147@users.noreply.github.com>
2025-06-20 14:40:53 +0000

    while

--- 05_iterations/01_for.js
+++ 05_iterations/01_for.js
@@ -32,11 +32,11 @@ for (let i=0; i < myArray.length;i++){
 
 for (let index = 1; index <= 20; index++) {
     if(index==5){
-        // console.log("5 detected");
+        console.log("5 detected");
         break
     
     }
-    // console.log(index);
+     console.log(index);
     
 }
 for (let index = 1; index <= 20; index++) {
